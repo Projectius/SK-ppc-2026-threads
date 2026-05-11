121e2a54523550dea1d14dfb2106b4a5b9dfacd2
Projectius <sprojectiustech@gmail.com>
2026-05-11 17:33:56 +0000

    wow format...

--- tasks/konstantinov_s_graham/tbb/src/ops_tbb.cpp
+++ tasks/konstantinov_s_graham/tbb/src/ops_tbb.cpp
@@ -26,8 +26,7 @@ bool KonstantinovAGrahamTBB::IsLowerAnchor(const std::vector<double> &xs, const 
   }
 
   return false;
-
-}  // namespace
+}
 
 KonstantinovAGrahamTBB::KonstantinovAGrahamTBB(const InType &in) {
   SetTypeOfTask(GetStaticTypeOfTask());
